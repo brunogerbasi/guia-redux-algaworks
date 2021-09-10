929ffc1157f914252c31c268155906ed81be3e01
Bruno Gerbasi <bruno_gerbasi@hotmail.com>
2021-09-10 13:56:49 +0100

    redux

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from "redux"
+import CalculatorReducer from "./Calculator/Calculator.reducer"
+
+
+const rootReducer = combineReducers({
+    calculator: CalculatorReducer
+})
+
+const store = createStore(rootReducer)
+
+export default store
